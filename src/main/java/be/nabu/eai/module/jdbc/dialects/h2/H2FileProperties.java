c69501033aeafc517d715f5f3eae38d2f266e861
Alexander Verbruggen <alex@nabu.be>
2020-11-13 22:33:05 +0100

    updated wizard

--- src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileProperties.java
+++ src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileProperties.java
@@ -1,12 +1,16 @@
 package be.nabu.eai.module.jdbc.dialects.h2;
 
-public class H2FileProperties {
-	private String file;
+import be.nabu.libs.types.api.annotation.Field;
 
-	public String getFile() {
-		return file;
+public class H2FileProperties {
+	// can't make this a File because it is a path on the server...
+	private String folder;
+
+	@Field(comment = "The folder where the database should be stored. Note that this is a file path on the server, not your developer machine.", defaultValue = "Defaults to the home folder")
+	public String getFolder() {
+		return folder;
 	}
-	public void setFile(String file) {
-		this.file = file;
+	public void setFolder(String folder) {
+		this.folder = folder;
 	}
 }
--- src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileWizard.java
+++ src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileWizard.java
@@ -1,6 +1,8 @@
 package be.nabu.eai.module.jdbc.dialects.h2;
 
+import be.nabu.eai.module.jdbc.pool.JDBCPoolArtifact;
 import be.nabu.eai.module.jdbc.pool.api.JDBCPoolWizard;
+import be.nabu.eai.repository.api.Entry;
 import be.nabu.eai.repository.resources.RepositoryEntry;
 
 public class H2FileWizard implements JDBCPoolWizard<H2FileProperties> {
@@ -21,13 +23,44 @@ public class H2FileWizard implements JDBCPoolWizard<H2FileProperties> {
 	}
 
 	@Override
-	public H2FileProperties load(RepositoryEntry entry) {
-		return new H2FileProperties();
+	public H2FileProperties load(JDBCPoolArtifact pool) {
+		H2FileProperties properties = new H2FileProperties();
+		String jdbcUrl = pool.getConfig().getJdbcUrl();
+		if (jdbcUrl != null && jdbcUrl.startsWith("jdbc:h2:file:")) {
+			properties.setFolder(jdbcUrl.substring("jdbc:h2:file:".length()));
+			return properties;
+		}
+		else {
+			return null;
+		}
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public void save(H2FileProperties instance, RepositoryEntry entry) {
-		// do nothing!
+	public JDBCPoolArtifact apply(Entry project, RepositoryEntry entry, H2FileProperties properties, boolean isNew, boolean isMain) {
+		try {
+			JDBCPoolArtifact existing = isNew ? new JDBCPoolArtifact(entry.getId(), entry.getContainer(), entry.getRepository()) : (JDBCPoolArtifact) entry.getNode().getArtifact();
+			if (isNew) {
+				existing.getConfig().setAutoCommit(false);
+			}
+			if (isMain) {
+				existing.getConfig().setContext(project.getId());
+			}
+			if (properties.getFolder() != null && !properties.getFolder().trim().isEmpty()) {
+				existing.getConfig().setJdbcUrl("jdbc:h2:file:" + properties.getFolder());
+			}
+			else {
+				existing.getConfig().setJdbcUrl("jdbc:h2:file:~/" + entry.getId());
+			}
+			Class clazz = H2Dialect.class;
+			existing.getConfig().setDialect(clazz);
+			existing.getConfig().setDriverClassName("org.h2.Driver");
+			return existing;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
+
 
 }
