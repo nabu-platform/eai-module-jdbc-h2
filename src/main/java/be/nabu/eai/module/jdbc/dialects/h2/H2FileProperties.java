2fbbf10a4a23d8fc03ed7096565ab514eda65658
Alexander Verbruggen <alex@nabu.be>
2020-11-15 14:13:32 +0100

    updated

--- src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileProperties.java
+++ src/main/java/be/nabu/eai/module/jdbc/dialects/h2/H2FileProperties.java
@@ -6,7 +6,7 @@ public class H2FileProperties {
 	// can't make this a File because it is a path on the server...
 	private String folder;
 
-	@Field(comment = "The folder where the database should be stored. Note that this is a file path on the server, not your developer machine.", defaultValue = "Defaults to the home folder")
+	@Field(comment = "The folder where the database should be stored. Note that this is a file path on the server, not your developer machine.\n\nThe default value should be enough for most cases so this can usually be left empty. Note that file-based h2 databases should not be used in clustered setups.", defaultValue = "Defaults to the home folder")
 	public String getFolder() {
 		return folder;
 	}
